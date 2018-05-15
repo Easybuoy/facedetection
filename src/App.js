8d085413e2bbf3048feff174f75d483a85817f3c
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-05-15 15:20:13 +0100

    [feature] facedetection

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import Particles from 'react-particles-js';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 200,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    move: {
+      enable: true,
+      speed: 10
+    }
+  },
+  interactivity: {
+    detect_on: 'window',
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'repulse'
+    }
+    } 
+}
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p> */}
+               <Particles className="particles"
+                  params={particleOptions}
+                  // style={{
+                  //   width: '100%',
+                  //   backgroundImage: `url(${logo})` 
+                  // }}
+                />
+        <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkForm />
+          
+          {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ImageLinkForm.css';
+const ImageLinkForm = () => {
+    return (
+        <div >
+           <p className="p3">
+                {'This Magic Brain Will Detect Faces In Your Picture. Give It A Try'}           
+           </p>
+
+            <div className="center"> 
+            <div className="form center pa4 br3 shadow"> 
+            <input type="text" className="f4 pa2 w-70 center " />
+            <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"> Detect </button>
+            </div>
+            </div>
+
+        </div>
+    );
+}
+
+
+export default ImageLinkForm;
